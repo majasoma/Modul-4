75363ba785f10c294e242fff5f3871455329aa3a
Maja soma <Majagrorudsoma@outlook.com>
2026-03-10 19:42:26 +0100

    oppgave 8 ny fil

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# Exercise 8: Simple Calculator Module
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
